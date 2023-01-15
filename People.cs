580b6a696146ee011d22eb699086ffe12d416524
SAMUEL ALEJANDRO HURTADO JIMENEZ <samuel.hurtado@nexti.com>
2023-01-15 11:39:12 -0500

    Completed

---
+++ People.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace StringVSstringBuilder
+{
+    internal class People
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FullNameWithString { get; set; }
+        public StringBuilder? FullNameWithStringBuilder { get; set; }
+
+        public IEnumerable<People> GetAll()
+        {
+            using StreamReader r = new StreamReader("@people.json");
+            People person = new People();
+            string json = r.ReadToEnd();
+
+            List<People> people = JsonConvert.DeserializeObject<List<People>>(json) ?? new();
+            return people;
+        }
+
+        public string AppendWithString(IEnumerable<People> people)
+        {
+            string fullname = string.Empty;
+            foreach (var person in people)
+            {
+                fullname += person.FirstName + " " + person.LastName + ", ";
+            }
+            return fullname;
+        }
+
+        public string AppendWithStringFormat(IEnumerable<People> people)
+        {
+            string fullname = string.Empty;
+            foreach (var person in people)
+            {
+                fullname = String.Format($"{0} {1} {2}", fullname, person.FirstName, person.LastName);
+            } 
+            return fullname;
+        }
+
+        public string AppendWithStringInterpolation(IEnumerable<People> people)
+        {
+            string fullname = string.Empty;
+            foreach (var person in people)
+            {
+                fullname = $"{fullname} {person.FirstName} {person.LastName}, ";
+            }
+            return fullname;
+        }
+
+        public string AppendWithStringBuilder(IEnumerable<People> people)
+        {
+            StringBuilder fullname = new();
+            foreach (var person in people)
+            {
+                fullname.AppendLine(person.FirstName).Append(person.LastName).Append(", ");
+            }
+            return fullname.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,73 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StringVSstringBuilder;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
 
+using StreamReader r = new StreamReader(@".\people.json");
+People person = new People();
+string json = r.ReadToEnd();
 
+List<People> people = JsonConvert.DeserializeObject<List<People>>(json) ?? new();
 
+int i = 0;
 
+while (i < 10)
+{
+    if (i > 0) people.AddRange(people);
+
+    Stopwatch timeMeasure = new();
+
+    #region TEST Append With String. example: fullname += person.FirstName + " " + person.LastName + ", ";
+    //TestAppendWithString(timeMeasure, people);
+    #endregion
+
+    #region TEST Append With String format. example: fullname = String.Format($"{0} {1} {2}", fullname, person.FirstName, person.LastName);
+    //TestAppendWithStringFormat(timeMeasure, people);
+    #endregion
+
+    #region TEST Append With String Interpolation. example: fullname = $"{fullname} {person.FirstName} {person.LastName}, ";
+    //TestAppendWithStringInterpolation(timeMeasure, people);
+    #endregion
+
+    #region TEST Append With String Builder. example: fullname.AppendLine(person.FirstName).Append(person.LastName).Append(", ");
+    TestAppendWithStringBuilder(timeMeasure, people);
+    #endregion
+
+    i++;
+}
+
+Console.WriteLine("Stop");
+
+void TestAppendWithString(Stopwatch timeMeasure, List<People> people)
+{
+    timeMeasure.Start();
+    person.AppendWithString(people);
+    timeMeasure.Stop();
+    Console.WriteLine($"TEST Append With String. Elements: {people.Count}. Time: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+}
+
+void TestAppendWithStringFormat(Stopwatch timeMeasure, List<People> people)
+{
+    timeMeasure.Start();
+    person.AppendWithStringFormat(people);
+    timeMeasure.Stop();
+    Console.WriteLine($"TEST Append With String format. Elements: {people.Count}. Time: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+}
+
+void TestAppendWithStringInterpolation(Stopwatch timeMeasure, List<People> people)
+{
+    timeMeasure.Start();
+    person.AppendWithStringInterpolation(people);
+    timeMeasure.Stop();
+    Console.WriteLine($"TEST Append With String Interpolation. Elements: {people.Count}. Time: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+}
+
+void TestAppendWithStringBuilder(Stopwatch timeMeasure, List<People> people)
+{
+    timeMeasure.Start();
+    person.AppendWithStringBuilder(people);
+    timeMeasure.Stop();
+    Console.WriteLine($"TEST Append With String Builder. Elements: {people.Count}. Time: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+}
